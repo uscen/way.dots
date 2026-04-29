6af42a237ed108abd75fbe5d01e324adb9b0bd91
uscen <farah.uscen@gmail.com>
2026-04-29 13:51:08 +0100

    fix(nvim): remove empty lines

--- void-dotfiles/cfg/nvim/plugin/other/treesitter.lua
+++ void-dotfiles/cfg/nvim/plugin/other/treesitter.lua
@@ -4,12 +4,10 @@
 Config.now_if_args(function()
   local ts_update = function() vim.cmd('TSUpdate') end
   Config.on_packchanged('tree-sitter', { 'update' }, ts_udpate, 'Update tree-sitter parsers')
-
   vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main', load = true },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', version = 'main' },
   })
-
   local ensure_languages = {
     'html',
     'css',
@@ -24,7 +22,6 @@ Config.now_if_args(function()
     'jq',
     'lua',
   }
-
   require('nvim-treesitter').install(ensure_languages)
   local filetypes = vim.iter(ensure_languages):map(vim.treesitter.language.get_filetypes):flatten():totable()
   vim.list_extend(filetypes, { 'markdown', 'pandoc' })
