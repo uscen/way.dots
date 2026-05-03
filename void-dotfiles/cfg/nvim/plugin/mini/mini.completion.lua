6ed8656a08480566dc6702d7ffe3e8355cea40ef
uscen <farah.uscen@gmail.com>
2026-05-04 00:52:53 +0100

    fix(nvim): add Keymap to mini.completion

--- void-dotfiles/cfg/nvim/plugin/mini/mini.completion.lua
+++ void-dotfiles/cfg/nvim/plugin/mini/mini.completion.lua
@@ -5,7 +5,7 @@ Config.now_if_args(function()
   local MiniCompletion = require('mini.completion')
   MiniCompletion.setup({
     lsp_completion = { source_func = 'omnifunc', auto_setup = false },
-    mappings = { force_fallback = '' },
+    mappings = { force_twostep = '<C-n>', force_fallback = '<C-S-n>', scroll_down = '<C-f>', scroll_up = '<C-b>' },
   })
   local on_attach = function(args) vim.bo[args.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp' end
   Config.new_autocmd('LspAttach', { callback = on_attach })
