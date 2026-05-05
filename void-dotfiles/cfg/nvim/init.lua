f7a97196c1905e93cf7f883f16dda25439782f05
uscen <farah.uscen@gmail.com>
2026-05-05 17:40:05 +0100

    fix(nvim): remove hipatterns

--- void-dotfiles/cfg/nvim/init.lua
+++ void-dotfiles/cfg/nvim/init.lua
@@ -20,19 +20,6 @@ _G.Config = {}
 
 -- Setup 'mini.misc' for access to `MiniMisc.safely()`: ==========================================
 local misc = require('mini.misc')
-local hipatterns = require('mini.hipatterns')
-hipatterns.setup({
-  highlighters = {
-    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
-
-    -- Highlight hex color strings (`#rrggbb`) using that color
-    hex_color = hipatterns.gen_highlighter.hex_color(),
-  },
-})
 
 -- Define package helpers: =======================================================================
 Config.now = function(f) misc.safely('now', f) end
