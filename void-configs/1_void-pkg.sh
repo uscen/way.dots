b8afbbe7dadb3bed9cb35239311a1ef83feefa54
uscen <farah.uscen@gmail.com>
2026-05-07 00:41:40 +0100

    fix(kvantum): sync colorscheme with my thorn theme

--- void-configs/1_void-pkg.sh
+++ void-configs/1_void-pkg.sh
@@ -12,7 +12,7 @@ sudo xbps-install -Sy void-repo-nonfree && sudo xbps-install -S \
     neovide obs mpv imv zathura zathura-pdf-mupdf firefox thunderbird nautilus gimp shotcut \
     udiskie exfatprogs ntfs-3g gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 \
     NetworkManager network-manager-applet wireless-regdb impala \
-    qt5-wayland qt6-wayland kvantum \
+    qt5-wayland qt6-wayland qt5ct qt6ct kvantum \
     pipewire wireplumber alsa-pipewire alsa-utils pavucontrol \
     bluez bluez-alsa libspa-bluetooth bluetui blueman \
     nodejs yarn pnpm lua-language-server \
