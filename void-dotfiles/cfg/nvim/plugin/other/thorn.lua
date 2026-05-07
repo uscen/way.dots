f47a9d27943bd41a4a32c4b6713e97de00e48b0b
uscen <farah.uscen@gmail.com>
2026-05-07 19:38:57 +0100

    fix(nvim): remove mini hipatterns

--- void-dotfiles/cfg/nvim/plugin/other/thorn.lua
+++ void-dotfiles/cfg/nvim/plugin/other/thorn.lua
@@ -5,9 +5,6 @@ Config.now(function()
   vim.pack.add({ 'https://github.com/jpwol/thorn.nvim' })
   require('thorn').setup({
     on_highlights = function(hl, p)
-      hl.PmenuSel = { bg = p.cursorline, bold = true }
-      hl.TreesitterContextLineNumber = { fg = p.number, bg = p.bg_float }
-      hl.LeapLabel = { fg = p.bg, bg = p.orange, bold = true }
       hl.MiniStatuslineDirectory = { bg = p.statusbar.bg, fg = p.gray }
       hl.MiniStatuslineFilename = { bg = p.statusbar.bg, fg = p.gray, bold = true }
     end,
