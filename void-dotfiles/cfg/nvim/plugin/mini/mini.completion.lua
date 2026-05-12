816b695293c1e62f1fe96446ecef3f9037370685
uscen <farah.uscen@gmail.com>
2026-05-12 23:05:07 +0100

    fix(nvim): update time

--- void-dotfiles/cfg/nvim/plugin/mini/mini.completion.lua
+++ void-dotfiles/cfg/nvim/plugin/mini/mini.completion.lua
@@ -4,6 +4,7 @@
 Config.now_if_args(function()
   local MiniCompletion = require('mini.completion')
   MiniCompletion.setup({
+    delay = { completion = 300, info = 200, signature = 150 },
     lsp_completion = { source_func = 'omnifunc', auto_setup = false },
     mappings = { force_twostep = '<C-n>', force_fallback = '<C-S-n>', scroll_down = '<C-f>', scroll_up = '<C-b>' },
   })
