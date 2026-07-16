f21a58dfa550f4fe1cf035d251ad95036b796add
uscen <farah.uscen@gmail.com>
2026-07-16 15:45:26 +0100

    fix(nvim): fix diagnostics style

--- void-dotfiles/cfg/nvim/plugin/core/options.lua
+++ void-dotfiles/cfg/nvim/plugin/core/options.lua
@@ -317,11 +317,38 @@ end)
 -- Diagnostics:                                                                   #
 -- ============================================================================== #
 local diagnostic_opts = {
+  severity_sort = false,
+  update_in_insert = false,
+  virtual_lines = false,
   underline = { severity = { min = 'HINT', max = 'ERROR' } },
   signs = { priority = 9999, severity = { min = 'ERROR', max = 'ERROR' } },
-  virtual_text = { current_line = true, virt_text_pos = 'eol_right_align', severity = { min = 'ERROR', max = 'ERROR' } },
-  virtual_lines = false,
-  update_in_insert = false,
+  float = {
+    prefix = '󰨓 ',
+    source = 'if_many',
+    style = 'minimal',
+    border = 'single',
+    header = '',
+    title = '󱓇 Diagnostics:',
+    title_pos = 'left',
+    max_height = 10,
+    max_width = 130,
+    focusable = false,
+    close_events = { 'CursorMoved', 'BufLeave', 'WinLeave', 'InsertEnter' },
+  },
+  virtual_text = {
+    current_line = true,
+    spacing = 2,
+    highlight = false,
+    prefix = '▎',
+    source = 'if_many',
+    virt_text_pos = 'eol_right_align',
+    severity = { min = 'ERROR', max = 'ERROR' },
+    format = function(diagnostic)
+      local icon = '→ '
+      local message = vim.split(diagnostic.message, '\n')[1]
+      return ('%s %s '):format(icon, message)
+    end,
+  },
 }
 -- Use `later()` to avoid sourcing `vim.diagnostic` on startup: ==================================
 Config.later(function() vim.diagnostic.config(diagnostic_opts) end)
