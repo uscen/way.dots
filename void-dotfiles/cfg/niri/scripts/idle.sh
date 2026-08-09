3a2852f6dd7626e4dd9a35a71eae60e2f49d77b5
uscen <farah.uscen@gmail.com>
2026-08-09 01:13:50 +0100

    fix(wm): make idle longer

--- void-dotfiles/cfg/niri/scripts/idle.sh
+++ void-dotfiles/cfg/niri/scripts/idle.sh
@@ -9,9 +9,9 @@ if pgrep -f "swayidle"; then
     notify-send "Auto Suspend" "Auto Suspend is now disabled"
 else
     swayidle -w \
-      timeout 300 'niri msg action power-off-monitors' \
-      timeout 330 'gtklock --daemonize' \
-      timeout 600 'doas zzz' \
+      timeout 600 'niri msg action power-off-monitors' \
+      timeout 630 'gtklock --daemonize' \
+      timeout 900 'doas zzz' \
       resume 'niri msg action power-off-monitors' \
       before-sleep 'gtklock --daemonize' &
     touch "$LOCKFILE"
