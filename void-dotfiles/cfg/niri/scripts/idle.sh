33c5e12a004fe2afa631277d52082fad64f80d07
uscen <farah.uscen@gmail.com>
2026-08-17 18:03:52 +0100

    fix(idle): don't show notification in login

--- void-dotfiles/cfg/niri/scripts/idle.sh
+++ void-dotfiles/cfg/niri/scripts/idle.sh
@@ -8,6 +8,13 @@ if pgrep -f "swayidle"; then
     rm -f "$LOCKFILE"
     notify-send "Auto Suspend" "Auto Suspend is now disabled"
 else
+    # Don't send notification in Login: ==========================================================
+    FIRST_RUN=true
+    if [ ! -f "$LOCKFILE" ]; then
+        FIRST_RUN=false
+    fi
+
+    # Start swayidle: ============================================================================
     swayidle -w \
       timeout 600 'niri msg action power-off-monitors' \
       timeout 630 'gtklock --daemonize' \
@@ -15,5 +22,9 @@ else
       resume 'niri msg action power-off-monitors' \
       before-sleep 'gtklock --daemonize' &
     touch "$LOCKFILE"
-    notify-send "Auto Suspend" "Auto Suspend is now enabled"
+
+    # Send notification but not when login: ======================================================
+    if [ "$FIRST_RUN" = false ]; then
+      notify-send "Auto Suspend" "Auto Suspend is now enabled"
+    fi
 fi
